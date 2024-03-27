9cb946aff30100737aee5cc91706499e980935fd
marcusluccas <lucilenebarbosamoura@gmail.com>
2024-03-26 21:32:22 -0300

    criado uma variavel do meu Rigid Body

--- flappy_plane/Assets/Scripts/PlayerController.cs
+++ flappy_plane/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //criando a minha variavel do meu rigidbody2d
+    private Rigidbody2D meuRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        meuRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
